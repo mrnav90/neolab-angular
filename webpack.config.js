d4143317897d2ae4b978cf5b0f1e6d85e82e132c
VietNa <viet.na@neo-lab.vn>
2017-05-08 16:14:28 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
       {
         test: /\.json$/,
         loader: 'json-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'exports-loader'
       }
     ]
   },
